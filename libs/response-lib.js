ed535f7d62d5ee55fa994f22bb6b4f4cbc92acb9
Mike Yamato <mike.yamato@mobileprogramming.com>
2019-04-23 16:29:39 -0700

    initial commit

---
+++ libs/response-lib.js
@@ -0,0 +1,21 @@
+// Return status code 200 and the newly created item
+export function success(body){
+  return buildResponse(200, body)
+}
+
+// Return status code 500 on error
+export function failure(body){
+  return buildResponse(500, body)
+}
+
+function buildResponse(statusCode, body){
+  return {
+    statusCode: statusCode,
+    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    },
+    body: JSON.stringify(body)
+  }
+}
